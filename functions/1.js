9795090300a53bfae6de9f7f81f6812e7076036d
shuklamanu246@hmail.com <shuklamanu246@hmail.com>
2024-12-04 13:13:56 +0530

    create fucntion and object folder with code-notes

---
+++ functions/1.js
@@ -0,0 +1,70 @@
+// FUNCTIONS->
+
+// when function define then we pass parameter
+
+// function addTwo(num1,num2){  -> parameter
+// 	console.log(num1+num2)
+// }
+// addTwo(5,6) -> argument
+
+// when function call then we pass argume
+// if we do not return anyting in fucntion it will cosnole undefined ,ex- console.log(functioncall)
+
+// function logOutMessage(username) {
+//     return `${username} logout succesfully`
+// }
+// console.log(logOutMessage("mayank"))     this is how we use function
+
+//  rest operator always use in function definition
+//  spred operator always use in function calling
+
+// fucntions in  array and Object
+// function calculatePrice(val1, val2, ...num1) {
+//     return num1;
+// }
+// console.log(calculatePrice(10, 20, 30, 40, 50))
+
+//funtions using object
+// const user = {
+//     name: "manu",
+//     age: 25
+// }
+// function handleobject(anyobject) {
+//     console.log(`username is ${anyobject.name} and age is ${anyobject.age}`);
+// }
+// handleobject(user)
+
+// const arr = [100, 200, 300, 300]
+
+// function thirdValuse(arr) {
+//     return [arr[1], arr[2]];
+// }
+// console.log(thirdValuse(arr))
+
+// SCOPES
+// 1. GLOBAL SCOPE outside the block anywhere in code
+// 2. LOCAL SCOPE
+// 3. BLOCK SCOPE code only written in {}
+
+// var c = 300
+// if (true) {
+//     var c = 30
+//     let a = 30
+//     const b = 25
+// console.log(a, b) this is only run as this is inside the block scope
+// }
+// console.log(a)
+// console.log(b)
+// console.log(c) this will run as var is global scope
+
+// child Function can access thier parent function
+// function parent() {
+//     let username = "manu";
+
+//     function child() {
+//         let name = "mayank";
+//         console.log(username)
+//     }
+//     child()
+// }
+// parent()
